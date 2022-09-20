c1551b91cda09745793eb9d371e1f596640aa934
William <wilindho@gmail.com>
2022-09-20 20:42:18 +0300

    more bs

--- api/src/main/java/pvp/api/datasource/Datasource.java
+++ api/src/main/java/pvp/api/datasource/Datasource.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Datasource {
+    // Some text
+
 
     @Bean
     @ConfigurationProperties("app.datasource")
